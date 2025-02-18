2ba705ada0eb90bc793c7ff1dde603ba2b34e5c9
GeanJS <gean.jair@gmail.com>
2025-02-18 17:59:39 -0300

    Committing new task.py with json and new tests

--- app/task.py
+++ app/task.py
@@ -1,67 +1,85 @@
-# Variable to control the auto-increment of the id
-last_id = 0
+import os
+import json
 
-# List to store dictionaries (simulating a database)
+last_id = 0
 tasks = []
+
+JSON_FILE = 'task.json'
+
+def load_tasks():
+    if os.path.exists(JSON_FILE):
+        with open(JSON_FILE, 'r') as file:
+            return json.load(file)
+    
+    return []
+
+
+def find_task_by_id(tasks: list, task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+    
+    return None
+
+
+def save_tasks(tasks: list):
+    with open(JSON_FILE, 'w') as file:
+        json.dump(tasks, file, indent=4)
+
+
+def get_next_id(tasks):
+    if tasks:
+        last_id = max(task['id'] for task in tasks)
+        return last_id + 1
+    return 1
 
 
 # Function to add a task 
-def add_task(task): 
+def add_task(tasks: list, task: dict):
     tasks.append(task)
+    save_tasks(tasks)
     print("Task added.")
     
     
 # Function to remove a task
-def remove_task():
+def remove_task(tasks):
     task_id = int(input("Enter the ID of the task to remove: "))
-    
-    task_to_remove = None
-    for task in tasks:
-        if task["id"] == task_id:
-            task_to_remove = task
-            break
-    
+    task_to_remove = find_task_by_id(tasks, task_id)
+
     if task_to_remove:
         tasks.remove(task_to_remove)
+        save_tasks(tasks)
         print(f"Task removed: {task_to_remove}")
     else:
         print("Task not found.")
         
         
 # Function to alter a task's status      
-def alter_task():
+def alter_task(tasks):
     task_id = int(input("Enter the ID of the task to alter: "))
-    
-    task_to_alter = None
-    for task in tasks:
-        if task["id"] == task_id:
-            task_to_alter = task
-            break
+    task_to_alter = find_task_by_id(tasks, task_id)
     
     if task_to_alter:
         new_status = input("Enter the new status for the task: ")
         task_to_alter["status"] = new_status
+        save_tasks(tasks)
         print(f"Task ID {task_id} updated. New status: {new_status}")
     else:
         print("Task not found.")
 
 # Function to list all tasks      
-def list_tasks():
+def list_tasks(tasks):
     if tasks:
-        print("Tasks:")
-        for i, task in enumerate(tasks):
-            print(f"{i + 1}. ID: {task['id']}, Description: {task['description']}, Status: {task['status']}")
+        print(json.dumps(tasks, indent=2))
     else:
-        print("No tasks found.")
+        print("Task list empty\n")
 
 
 # Function to create a task
-def create_task():
-    global last_id
-    last_id += 1
-    
+def create_task(tasks):
+    new_id = get_next_id(tasks)
     task = {
-        "id": last_id,
+        "id": new_id,
         "description": input("Please provide the task description: "),
         "status": input("Please provide the current status of the task: ")
     }
@@ -83,24 +101,25 @@ def menu():
     
 # Main function of the app   
 def main():
+    tasks = load_tasks()
     while True:
         option = menu()
         print()
         
         match option:
             case '1':
-                task = create_task()
-                add_task(task)
+                task = create_task(tasks)
+                add_task(tasks, task)
                 print()
             
             case '2':
-               remove_task()
+               remove_task(tasks)
                     
             case '3':
-                alter_task()
+                alter_task(tasks)
             
             case '4':
-                list_tasks()
+                list_tasks(tasks)
             
             case '5':
                 break
--- app/test_task.py
+++ app/test_task.py
@@ -1,6 +1,7 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, mock_open, call
 import task
+import json
 
 mock = {
             'id': 1,
@@ -8,62 +9,75 @@ mock = {
             'status': 'feito'
         }
 
+mock_list = [mock]
+
 class TestTask(unittest.TestCase):
     def tearDown(self):
         task.tasks = []
+        mock_list.clear()
+        mock_list.append(mock)
     
     @patch('builtins.print', return_value='')
-    def test_should_add_task_to_list(self, mock_print):
-        task.add_task(mock)
+    @patch('task.save_tasks')
+    @patch('task.load_tasks', return_value=[])
+    def test_should_add_task_to_list(self, mock_load_tasks, mock_save_tasks, mock_print):
+        task.add_task(task.tasks, mock)
         self.assertIn(mock, task.tasks)
+        mock_save_tasks.assert_called_once_with(task.tasks)
+        mock_print.assert_called_once_with("Task added.")
     
-    @patch('builtins.print', return_value='')
+    
+    @patch('builtins.print')
     @patch('builtins.input', return_value='1')
-    def test_should_remove_task_from_list(self, mock_input, mock_print):
-        task.add_task(mock)
-        task.remove_task()
-        self.assertNotIn(mock, task.tasks)
+    @patch('task.save_tasks')
+    @patch('task.load_tasks', return_value=mock_list)
+    def test_should_remove_task_from_list(self, mock_load_tasks, mock_save_tasks, mock_input, mock_print):
+        task.remove_task(mock_list)
+        self.assertNotIn(mock, mock_list)
+        mock_save_tasks.assert_called_once_with(mock_list)
+        mock_print.assert_called_once_with(f"Task removed: {mock}")
     
     
     @patch('builtins.print', return_value='')
     @patch('builtins.input', side_effect=['1', 'arquivado'])
-    def test_should_alter_task(self, mock_print, mock_input):
-        task.add_task(mock)
-        task.alter_task()
-        self.assertEqual('arquivado', task.tasks[0]["status"])
+    @patch('task.save_tasks')
+    @patch('task.load_tasks', return_value=mock_list)
+    def test_should_alter_task(self, mock_load_tasks, mock_save_tasks, mock_input, mock_print):
+        task.alter_task(mock_list)
+        self.assertEqual('arquivado', mock_list[0]["status"])
+        mock_save_tasks.assert_called_once_with(mock_list)
+        mock_print.assert_called_once_with(f"Task ID 1 updated. New status: arquivado")
     
     
     @patch('builtins.print', return_value='')
     @patch('builtins.input', side_effect=['2'])
-    def test_should_not_found_when_alter_task(self, mock_input, mock_print):
-        task.add_task(mock)
-        task.alter_task()
+    @patch('task.load_tasks', return_value=mock_list)
+    def test_should_not_found_when_alter_task(self, mock_load_tasks, mock_input, mock_print):
+        task.alter_task(mock_list)
         mock_print.assert_called_with("Task not found.")
 
     
     @patch('builtins.print')
-    def test_should_show_all_tasks(self, mock_print):
-        task.add_task(mock)
-        task.list_tasks()
+    @patch('task.load_tasks', return_value=mock_list)
+    def test_should_show_all_tasks(self, mock_load_tasks, mock_print):
+        task.list_tasks(mock_list)
         expected_calls = [
-            unittest.mock.call("Tasks:"),
-            unittest.mock.call("1. ID: 1, Description: Cozinhar, Status: arquivado")
+            call(json.dumps(mock_list, indent=2))
         ]
-
         mock_print.assert_has_calls(expected_calls, any_order=False)
     
     
     @patch('builtins.print')
-    def test_should_show_no_tasks(self, mock_print):
-        task.list_tasks()
-        
-        mock_print.assert_called_once_with("No tasks found.")
+    @patch('task.load_tasks', return_value=[])
+    def test_should_show_no_tasks(self, mock_load_tasks, mock_print):
+        task.list_tasks([])
+        mock_print.assert_called_once_with("Task list empty\n")
     
     
     @patch('builtins.input', side_effect=['Estudar', 'não feito'])
-    def test_should_create_task(self, mock_input):
-        new_task = task.create_task()
-        
+    @patch('task.load_tasks', return_value=[])
+    def test_should_create_task(self, mock_load_tasks, mock_input):
+        new_task = task.create_task([])
         self.assertEqual({'id': 1, 'description': 'Estudar', 'status': 'não feito' }, new_task)
     
 if __name__ == '__main__':
